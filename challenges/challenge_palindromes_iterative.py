0c4e51ccd8050dd104db2ca78dd77a94defd5f1b
Joao Antonio <joaopsilva.dev@gmail.com>
2022-11-19 11:33:10 -0300

    feat: :sparkles: requisito 6 completo
    função palindromo iterativo completa

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,13 @@
 def is_palindrome_iterative(word):
-    ...
-#     if len(word) == 0:
-#         return False
-#     elif low_index == high_index:
-#         return True
-#     elif low_index + 1 == high_index:
-#         return word[low_index] == word[high_index]
-#     else:
-#         return word[low_index] == word[high_index] and
-#   is_palindrome_recursive(word, low_index + 1, high_index -1 )
+    high_index = len(word) - 1
+    qtd_equals = 0
+    for low_index in range(len(word)):
+        if word[low_index] == word[high_index]:
+            qtd_equals += 1
+        if low_index >= high_index:
+            break
+        high_index = high_index - 1
+    if qtd_equals == (len(word) // 2) + 1:
+        return True
+    else:
+        return False
