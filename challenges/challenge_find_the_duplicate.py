c4f2f9628e6fca3921b8cce580f384beee2cc179
Joao Antonio <joaopsilva.dev@gmail.com>
2022-11-19 12:38:33 -0300

    feat: :sparkles: requisito 5 completo
    função find duplicate completa

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,33 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) == 0:
+        return False
+    elif type(nums) != list:
+        return False
+    else:
+        nums.sort()
+        return verify_duplicate(nums)
+
+
+def verify_duplicate(nums):
+    duplicate = False
+    for index in range(len(nums)):
+        if index + 1 == len(nums):
+            break
+        elif not verify_number(nums[index]):
+            duplicate = False
+            break
+        elif nums[index] == nums[index + 1]:
+            duplicate = nums[index]
+            break
+    return duplicate
+
+
+def verify_number(number):
+    if type(number) != int:
+        return False
+
+    elif number < 0:
+        return False
+
+    else:
+        return True
